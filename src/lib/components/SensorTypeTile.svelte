1188f56cf6a08146fbf978c745d2ef2c52f14af0
KomorowskiKuba <jakubjankomorowski@gmail.com>
2022-04-08 22:43:46 +0200

    Added sensor info and type form

---
+++ src/lib/components/SensorTypeTile.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let icon_name = "";
+    export let path = "";
+    export let text = "";
+    export let fun = "";
+    export let type = "";
+
+    function testIsChanged() {
+        type = fun;
+    }
+</script>
+
+<a href={path} on:click={testIsChanged}>
+    <span class="material-icons" style="font-size: 110px;">{icon_name}</span>
+</a>
+<p>
+    <b>{text}</b>
+</p>
