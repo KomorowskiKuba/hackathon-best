646ab071b139763079893e55795072c17e4f2e65
Kuba Smolaga <smolagakuba@gmail.com>
2022-04-09 15:19:43 +0200

    made more sensors

--- src/lib/stores/SensorStore.js
+++ src/lib/stores/SensorStore.js
@@ -3,7 +3,7 @@ import { writable } from "svelte/store";
 import { browser } from '$app/env'
 
 const dataFromStorage = browser && JSON.parse(window.localStorage.getItem('sensors'))
-const SensorStore = writable(dataFromStorage || generateManySensors(100))
+const SensorStore = writable(dataFromStorage || generateManySensors(500))
 SensorStore.subscribe(sensors => {
     if (browser) window.localStorage.setItem('sensors', JSON.stringify(sensors));
 });
