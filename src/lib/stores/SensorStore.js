ee0b659c93123a1768907b8dcbbc09a25c8307c1
Kuba Smolaga <smolagakuba@gmail.com>
2022-04-09 13:18:32 +0200

    sensor store now sotres in local storage

--- src/lib/stores/SensorStore.js
+++ src/lib/stores/SensorStore.js
@@ -1,6 +1,12 @@
 import { generateManySensors } from "$lib/sensorGenerator";
 import { writable } from "svelte/store";
+import { browser } from '$app/env'
 
-const SensorStore = writable(generateManySensors(100))
+
+const dataFromStorage = browser && JSON.parse(window.localStorage.getItem('sensors'))
+const SensorStore = writable(dataFromStorage || generateManySensors(100))
+SensorStore.subscribe(sensors => {
+    if (browser) window.localStorage.setItem('sensors', JSON.stringify(sensors));
+});
 
 export default SensorStore;
--- src/routes/sensors/[id].svelte
+++ src/routes/sensors/[id].svelte
@@ -3,69 +3,71 @@
 	import Chart from '$lib/components/charts/Chart.svelte';
 	import { page } from '$app/stores';
 
-	const id = parseInt($page.params['id']);
-	const sensor = $SensorStore[id];
+	$: id = parseInt($page.params['id']);
+	$: sensor = $SensorStore[id] || {};
 </script>
 
-<section style="margin-top: -4rem;">
-	<header>
-		<h2>Sensor Data</h2>
-	</header>
-	<aside>
-		<h3 style="display: flex; align-items: center;">
-			<span class="material-icons" style="font-size: inherit;"> info </span>
-			&nbsp;Information
-		</h3>
-		<ul>
-			<li>Id: <a href="/">{id}</a></li>
-			<li>Name: <a href="/">{sensor.name}</a></li>
-			<li>Country: <a href="/">{sensor.country}</a></li>
-			<li>City: <a href="/">{sensor.city}</a></li>
-			<li>Category: <a href="/">{sensor.category}</a></li>
-			<li>Author: <a href="/">{sensor.author}</a></li>
-		</ul>
-	</aside>
-	<aside>
-		<h3>
-			<span class="material-icons" style="font-size: inherit;"> timeline </span>
-			&nbsp;Display Options
-		</h3>
-		Span:
-		<select name="time-span">
-			<option>1h</option>
-			<option>24h</option>
-			<option>48h</option>
-			<option>7d</option>
-			<option>30d</option>
-			<option>1 year</option>
-		</select>
-		Scale:
-		<select>
-			<option>Linear</option>
-			<option>Logarithmic</option>
-		</select>
-	</aside>
-	<aside>
-		<h3>
-			<span class="material-icons" style="font-size: inherit;"> star </span>
-			Additional Options
-		</h3>
-		<a href="/" class="small-button">
-			<em>
-				<span class="material-icons"> file_download </span>
-				Download CSV
-			</em>
-		</a>
-		<br />
-		<a href="/" class="small-button">
-			<em>
-				<span class="material-icons"> paid </span>
-				Support the provider
-			</em>
-		</a>
-	</aside>
-</section>
-<Chart />
+{#if !!sensor}
+	<section style="margin-top: -4rem;">
+		<header>
+			<h2>Sensor Data</h2>
+		</header>
+		<aside>
+			<h3 style="display: flex; align-items: center;">
+				<span class="material-icons" style="font-size: inherit;"> info </span>
+				&nbsp;Information
+			</h3>
+			<ul>
+				<li>Id: <a href="/">{id}</a></li>
+				<li>Name: <a href="/">{sensor.name}</a></li>
+				<li>Country: <a href="/">{sensor.country}</a></li>
+				<li>City: <a href="/">{sensor.city}</a></li>
+				<li>Category: <a href="/">{sensor.category}</a></li>
+				<li>Author: <a href="/">{sensor.author}</a></li>
+			</ul>
+		</aside>
+		<aside>
+			<h3>
+				<span class="material-icons" style="font-size: inherit;"> timeline </span>
+				&nbsp;Display Options
+			</h3>
+			Span:
+			<select name="time-span">
+				<option>1h</option>
+				<option>24h</option>
+				<option>48h</option>
+				<option>7d</option>
+				<option>30d</option>
+				<option>1 year</option>
+			</select>
+			Scale:
+			<select>
+				<option>Linear</option>
+				<option>Logarithmic</option>
+			</select>
+		</aside>
+		<aside>
+			<h3>
+				<span class="material-icons" style="font-size: inherit;"> star </span>
+				Additional Options
+			</h3>
+			<a href="/" class="small-button">
+				<em>
+					<span class="material-icons"> file_download </span>
+					Download CSV
+				</em>
+			</a>
+			<br />
+			<a href="/" class="small-button">
+				<em>
+					<span class="material-icons"> paid </span>
+					Support the provider
+				</em>
+			</a>
+		</aside>
+	</section>
+	<Chart />
+{/if}
 
 <style>
 	.small-button em {
