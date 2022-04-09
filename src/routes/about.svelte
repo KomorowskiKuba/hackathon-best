ac838214e906248854d63affb5840484894e9ed7
KomorowskiKuba <jakubjankomorowski@gmail.com>
2022-04-09 09:50:31 +0200

    Added about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,33 +1,13 @@
-<h1>About</h1>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus turpis urna, tincidunt a sem
-	venenatis, egestas dapibus ipsum. In tempor nulla ut erat aliquam, ac egestas sem pretium.
-	Vestibulum dignissim mauris nulla, ut tempor neque laoreet ut. Curabitur mattis pulvinar ex, at
-	laoreet dolor condimentum non. Nunc tincidunt accumsan gravida. Maecenas finibus arcu mauris.
-	Proin semper hendrerit ultrices. Quisque a purus a urna mollis bibendum eget vitae odio. Fusce
-	bibendum enim eu euismod commodo.
-</p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus turpis urna, tincidunt a sem
-	venenatis, egestas dapibus ipsum. In tempor nulla ut erat aliquam, ac egestas sem pretium.
-	Vestibulum dignissim mauris nulla, ut tempor neque laoreet ut. Curabitur mattis pulvinar ex, at
-	laoreet dolor condimentum non. Nunc tincidunt accumsan gravida. Maecenas finibus arcu mauris.
-	Proin semper hendrerit ultrices. Quisque a purus a urna mollis bibendum eget vitae odio. Fusce
-	bibendum enim eu euismod commodo.
-</p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus turpis urna, tincidunt a sem
-	venenatis, egestas dapibus ipsum. In tempor nulla ut erat aliquam, ac egestas sem pretium.
-	Vestibulum dignissim mauris nulla, ut tempor neque laoreet ut. Curabitur mattis pulvinar ex, at
-	laoreet dolor condimentum non. Nunc tincidunt accumsan gravida. Maecenas finibus arcu mauris.
-	Proin semper hendrerit ultrices. Quisque a purus a urna mollis bibendum eget vitae odio. Fusce
-	bibendum enim eu euismod commodo.
-</p>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus turpis urna, tincidunt a sem
-	venenatis, egestas dapibus ipsum. In tempor nulla ut erat aliquam, ac egestas sem pretium.
-	Vestibulum dignissim mauris nulla, ut tempor neque laoreet ut. Curabitur mattis pulvinar ex, at
-	laoreet dolor condimentum non. Nunc tincidunt accumsan gravida. Maecenas finibus arcu mauris.
-	Proin semper hendrerit ultrices. Quisque a purus a urna mollis bibendum eget vitae odio. Fusce
-	bibendum enim eu euismod commodo.
-</p>
+<header>
+	<h1>About</h1>
+	<p>
+		Our company is all about data. Open access to data is the most important thing for us. Our website collects data from sensors and shares it to the others.
+		But what is done with data that people do not see is done to our customers. But some consumers of the iPhone are still confused by the concept.
+		I said to myself "who cares what's happening down there in the world but when are we going to put data in?
+		And that became my point of conversation with Michael Eisner, chief executive officer of Motorola.
+		"We think we are a technology company. There is no other way to say that. We have never made any acquisitions in a single day. In the past we made big deals, but it wasn't until recently that we made major changes like this, and today we made a lot of big acquisitions with Google," Eisner said.
+		"We do have some big acquisitions, but we are committed to data in whatever way we can … This is not, and never has been, an app, but this is the right way to go to make that happen."
+		That includes a slew of Android smartphones out there, and some of that data is coming from Motorola. And Motorola has invested significant resources into making it as easy as possible to use and to provide a better and more user experience than Android.
+		The problem is, some users are going to see the "it" moment and have long been confused by its implications.
+	</p>
+</header>
